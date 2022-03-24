7a7ab7b0d897854652a3883ed443d40678ad4ee7
makstravm <maks7294@gmail.com>
2022-03-24 15:03:32 +0200

    added setup React

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Name } from "./components/Name.jsx";
+
+const App = () => (
+  <>
+    <Name name="Bob" />
+    <Name name="Jane" />
+  </>
+);
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   context: path.join(__dirname, "src"),
   mode: "development",
   entry: {
-    index: "./index.js",
+    index: ["@babel/polyfill", "./index.jsx"],
   },
   output: {
     filename: "[name].bundle.js",
@@ -15,16 +18,36 @@ module.exports = {
   devServer: {
     static: "./dist",
   },
+  devtool: "source-map",
   plugins: [
     new HtmlWebpackPlugin({
       template: "index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: `[name].[hash].css`,
+    }),
   ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+    minimizer: [new OptimizeCssAssetWebpackPlugin(), new TerserWebpackPlugin()],
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -33,6 +56,28 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
       },
     ],
   },
