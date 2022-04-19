46b1edd143a9f706814b90e088a57aaff25e9da9
makstravm <maks7294@gmail.com>
2022-04-19 11:04:22 +0300

    rename className fieldsItem

--- src/components/FieldsList/FieldsItem/FieldsItem.tsx
+++ src/components/FieldsList/FieldsItem/FieldsItem.tsx
@@ -25,7 +25,7 @@ export const FieldsItem = ({
   return (
     <Paper>
       <Grid
-        className="fieldsItem"
+        className="fields-item"
         sx={{
           margin: "0 5px 10px 5px",
           padding: " 0 15px 0 15px",
@@ -36,7 +36,7 @@ export const FieldsItem = ({
         alignItems="center"
         justifyContent="space-between"
       >
-        <Grid className="fieldsItem__name" item xs={4}>
+        <Grid className="fields-item__name" item xs={4}>
           <Typography>{name}</Typography>
           {required && <Typography color="red">*</Typography>}
           {type === SELECT && (
@@ -51,7 +51,7 @@ export const FieldsItem = ({
         <Grid />
       </Grid>
       {showOptions && (
-        <Stack spacing={1} mb={2}>
+        <Stack spacing={1} sx={{ marginTop: "-10px" }} mb={2}>
           <Divider />
           {options.map(({ value, id }) => (
             <Typography variant="subtitle2" pl={5} key={id}>
