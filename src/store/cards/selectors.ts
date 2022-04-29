0287aa2ea079bc3e946dc6020e6ea441529acb11
makstravm <maks7294@gmail.com>
2022-04-29 18:24:05 +0300

    changed selector cards

--- src/store/cards/selectors.ts
+++ src/store/cards/selectors.ts
@@ -1,22 +1,45 @@
+import { cardsGroupByName } from "constants/cardsGroupByName";
 import { createSelector } from "reselect";
 
 import { RootStateType } from "..";
 
-import { GroupOneCardsListsType } from "./types";
+import { CardType } from "./types";
+
+const { ALL } = cardsGroupByName;
 
 const getCardsList = (state: RootStateType) => state?.cards?.cardsList;
 
+const getFields = (state: RootStateType) => state?.fields?.fieldsList;
+
 export const selectGroupCardsList = (groupName: string) =>
-  createSelector(getCardsList, (cardsList) => {
-    const groupCardsList = cardsList
-      .map((val) => (groupName !== "All" ? `${val[groupName]}` : groupName))
-      .reduce((acc: GroupOneCardsListsType, val, i) => {
-        acc[val] = [...(acc?.[val] || []), cardsList[i]];
+  createSelector(getCardsList, getFields, (cardsList, fieldsList) => {
+    const groupCardsList: { [k: string]: CardType[] | [] } = {};
 
-        return acc;
-      }, {});
+    if (groupName === ALL) {
+      groupCardsList[groupName] = cardsList;
+    } else {
+      const findField = fieldsList?.find((field) => field?.name === groupName);
 
-    const sortGroupCardsList = Object.entries(groupCardsList).sort();
+      if (findField?.type === "checkbox") {
+        groupCardsList.true = [];
+        groupCardsList.false = [];
+      }
+      if (findField?.type === "select") {
+        findField?.options?.forEach(({ value }) => {
+          groupCardsList[value] = [];
+        });
+      }
+      if (Object.keys(groupCardsList).length) {
+        cardsList?.forEach((element) => {
+          const key = element[groupName].toString();
 
-    return sortGroupCardsList;
+          groupCardsList[key] = [...groupCardsList[key], element];
+        });
+      }
+      const sortGroupCardsList = Object.entries(groupCardsList).sort();
+
+      return sortGroupCardsList;
+    }
+
+    return Object.entries(groupCardsList);
   });
