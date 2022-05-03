be2e80b527bd3b53b138f86b266fc9b13143850f
makstravm <maks7294@gmail.com>
2022-05-03 18:27:43 +0300

    remove some types

--- src/store/auth/types.ts
+++ src/store/auth/types.ts
@@ -1,13 +1,3 @@
-export enum AuthActionTypes {
-  LOGIN_STARTED = "LOGIN_STARTED",
-  LOGIN_SUCCESS = "LOGIN_SUCCESS",
-  LOGIN_FAILURE = "LOGIN_FAILURE",
-  LOGOUT_SUCCESS = "LOGOUT_SUCCESS",
-  REGISTER_STARTED = "REGISTER_STARTED",
-  REGISTER_SUCCESS = "REGISTER_SUCCESS",
-  REGISTER_FAILURE = "REGISTER_FAILURE",
-}
-
 export type AuthUserType = {
   name: string;
   email: string;
@@ -27,43 +17,3 @@ export type AuthResponseType = {
   accessToken: string | null;
   user: AuthUserType | null;
 };
-
-export type LoginStartedType = {
-  type: AuthActionTypes.LOGIN_STARTED;
-};
-
-export type LoginSuccessType = {
-  type: AuthActionTypes.LOGIN_SUCCESS;
-  payload: AuthUserType;
-};
-
-export type LogoutSuccessType = {
-  type: AuthActionTypes.LOGOUT_SUCCESS;
-};
-
-export type LoginFailureType = {
-  type: AuthActionTypes.LOGIN_FAILURE;
-  payload: string | null;
-};
-
-export type RegisterStartedType = {
-  type: AuthActionTypes.REGISTER_STARTED;
-};
-
-export type RegisterSuccessType = {
-  type: AuthActionTypes.REGISTER_SUCCESS;
-};
-
-export type RegisterFailureType = {
-  type: AuthActionTypes.REGISTER_FAILURE;
-  payload: string | null;
-};
-
-export type AuthReducerActionsTypes =
-  | RegisterFailureType
-  | RegisterSuccessType
-  | RegisterStartedType
-  | LoginFailureType
-  | LoginSuccessType
-  | LoginStartedType
-  | LogoutSuccessType;
