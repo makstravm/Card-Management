1737f94effc3a096eb4706309770692e439ea0a0
makstravm <maks7294@gmail.com>
2022-06-21 18:48:22 +0300

    added tests for other error variants

--- src/services/tests/errorBoundary.test.ts
+++ src/services/tests/errorBoundary.test.ts
@@ -3,14 +3,47 @@ import { errorBoundary } from "../errorBoundary";
 
 jest.mock("utils/toast");
 describe("errorBoudary", () => {
-  it("should be ", () => {
+  it("should be show error when status 404", () => {
     const err = {
       status: 404,
       data: "Error",
+      statusText: "Error StatusText",
+    };
+
+    errorBoundary(err);
+    expect(fn.notifyError).toHaveBeenCalledWith("Error StatusText");
+  });
+
+  it("should be show error when status 400", () => {
+    const err = {
+      status: 400,
+      data: "Error Data",
       statusText: "Error",
     };
 
     errorBoundary(err);
-    expect(fn.notifyError).toHaveBeenCalledWith("Error");
+    expect(fn.notifyError).toHaveBeenCalledWith("Error Data");
+  });
+
+  it("should be show error when status 401", () => {
+    const err = {
+      status: 401,
+      data: "Error Data",
+      statusText: "Error",
+    };
+
+    errorBoundary(err);
+    expect(fn.notifyError).toHaveBeenCalledWith("Error Data");
+  });
+
+  it("should be show error when any other status ", () => {
+    const err = {
+      status: 405,
+      data: "Error Data",
+      statusText: "Error",
+    };
+
+    errorBoundary(err);
+    expect(fn.notifyError).toHaveBeenCalledWith("Error Data");
   });
 });
