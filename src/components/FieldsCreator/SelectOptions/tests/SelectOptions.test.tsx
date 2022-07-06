d4c852cdfcdebcc911a131e37e1a89c24d394b14
makstravm <maks7294@gmail.com>
2022-07-06 15:02:57 +0300

    added unit test "should been checked dublicate values" for SelectOption component

--- src/components/FieldsCreator/SelectOptions/tests/SelectOptions.test.tsx
+++ src/components/FieldsCreator/SelectOptions/tests/SelectOptions.test.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Formik } from "formik";
 import userEvent from "@testing-library/user-event";
+
+import { switchedFieldCreateValidation } from "../../../../helpers/optionsValidSchema";
 import { SelectOptions } from "..";
 
 const initialValue = {
@@ -20,7 +22,11 @@ const initialValue = {
 describe("SelectOptions component without props", () => {
   beforeEach(() => {
     render(
-      <Formik initialValues={initialValue} onSubmit={() => {}}>
+      <Formik
+        initialValues={initialValue}
+        onSubmit={() => {}}
+        validationSchema={switchedFieldCreateValidation("select")}
+      >
         {(props) => <SelectOptions options="options" formik={props} />}
       </Formik>
     );
@@ -59,5 +65,12 @@ describe("SelectOptions component without props", () => {
     const input = screen.getByTestId("input-option-0") as HTMLInputElement;
 
     expect(input.value).toBe("John");
+  });
+  it("should been checked dublicate values", async () => {
+    await userEvent.type(screen.getByTestId("input-option-0"), "John");
+    expect(screen.queryAllByText(/duplicate/i)[0]).toBeFalsy();
+    await userEvent.type(screen.getByTestId("input-option-1"), "John");
+
+    expect(screen.queryAllByText(/duplicate/i)[0]).toBeTruthy();
   });
 });
